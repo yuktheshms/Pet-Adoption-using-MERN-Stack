2b416b39d6baa5723e8232b4c2b490f7b126f399
Yukthesh M S <142963650+yuktheshms@users.noreply.github.com>
2024-10-23 14:32:29 +0530

    Update CustionFetch.jsx

--- fornt/src/axios/CustionFetch.jsx
+++ fornt/src/axios/CustionFetch.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 // const url = "http://localhost:5000";
-const url = "http://localhost:5000";
+const url = "https://pet-adoption-using-mern-stack-cgq5-backend.vercel.app";
 // const URL= "https://getyourstuffbackend.onrender.com"
 const CustomFetch = axios.create({
   baseURL: url,
